bec48f67ef005d115a073c8a7c6cd8b247f17a0b
Nataly Moskalenko <y.natali.moskalenko@gmail.com>
2021-03-10 15:55:11 +0200

    feat: add functions min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
 exports.min = function min (array) {
-  return 0;
+    return  (!array || array.length == 0) ? 0 : array.reduce((min, cur) => min <= cur ? min : cur);
 }
 
 exports.max = function max (array) {
-  return 0;
+    return  (!array || array.length == 0) ? 0 : array.reduce((max, cur) => max >= cur ? max : cur);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return (!array || array.length == 0) ? 0 : array.reduce((prev, cur) => prev + cur) / array.length;
 }
